42c27a6ba870ada580faaef7d1f5635c52655b92
jasongtsk <payouts_rink_0a@icloud.com>
2026-04-22 15:12:01 +0100

    feat: add cors

--- Ballot.API/Program.cs
+++ Ballot.API/Program.cs
@@ -1,5 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var allowedOrigins = new List<string> { "http://localhost:3000", "http://127.0.0.1:3000" };
+var frontendUrl = builder.Configuration["Frontend__Url"];
+if (!string.IsNullOrEmpty(frontendUrl))
+    allowedOrigins.Add(frontendUrl);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(allowedOrigins.ToArray())
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
@@ -19,6 +35,7 @@ if (app.Environment.IsDevelopment())
 app.UseHealthChecks("/health");
 
 app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
 app.MapControllers();
 
 app.Run();
